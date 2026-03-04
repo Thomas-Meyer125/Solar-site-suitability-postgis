9b705585b47359a721aaf1343222e81dd1b20777
Thomas J. Meyer <tmeyer125@gmail.com>
2026-03-04 13:18:37 -0500

    Update 03_slope_analysis.sql

--- sql/03_slope_analysis.sql
+++ sql/03_slope_analysis.sql
@@ -1,1 +1,11 @@
+-- 03_slope_analysis.sql
 
+-- Create slope raster in degrees
+CREATE TABLE dem_slope AS
+SELECT
+    ST_Slope(rast, 1, '32BF', 'DEGREES') AS rast
+FROM dem;
+
+CREATE INDEX dem_slope_gix 
+ON dem_slope 
+USING GIST (ST_ConvexHull(rast));
