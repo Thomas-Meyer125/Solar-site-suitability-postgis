40cfa58034c0191d45e8df28b94cdfb1d93ac440
Thomas J. Meyer <tmeyer125@gmail.com>
2026-03-04 13:19:03 -0500

    Update 04_zonal_statistics.sql

--- sql/04_zonal_statistics.sql
+++ sql/04_zonal_statistics.sql
@@ -1,1 +1,19 @@
+-- 04_zonal_statistics.sql
 
+-- Calculate mean slope within each parcel
+
+CREATE TABLE parcel_slope_stats AS
+SELECT
+    p.gid,
+    p.geom,
+    p.area_m2,
+    (ST_SummaryStats(
+        ST_Clip(s.rast, p.geom)
+    )).mean AS mean_slope
+FROM parcels_no_flood p
+JOIN dem_slope s
+ON ST_Intersects(p.geom, ST_ConvexHull(s.rast));
+
+CREATE INDEX parcel_slope_stats_gix 
+ON parcel_slope_stats 
+USING GIST (geom);
